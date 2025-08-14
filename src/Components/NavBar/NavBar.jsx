c21911d86bc1c3089c9ef8cd0a408d727522e7af
rusinduss <rusindunm@gmail.com>
2025-08-15 01:03:05 +0530

    after adding new projects

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,41 +1,36 @@
-
 import { useState } from "react";
-import menuIcon from "../../assets/menuIcon.png"
- import closeIcon from "../../assets/closeIcon.png" 
+import menuIcon from "../../assets/menuIcon.png";
+import closeIcon from "../../assets/closeIcon.png";
 
 import "./NavBar.css";
 
 export const NavBar = () => {
   const [menuopen, setMenuopen] = useState(false);
+
   return (
     <nav className="navbar">
       <a className="title" href="/">
         Portfolio
       </a>
+
       <div className="menu">
+        {/* Menu button for mobile */}
         <img
           className="menubtn"
-          src={
-            menuopen
-              ? menuIcon
-              : closeIcon
-          }
+          src={menuopen ? closeIcon : menuIcon}
           alt="menu-button"
-          onClick={()=>setMenuopen(!menuopen)}
+          onClick={() => setMenuopen(!menuopen)}
         />
-        <ul className= {menuopen ? "menuopen": "menuItems"} onClick={()=>setMenuopen(true)} >
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#Experience">Experiences</a>
-          </li>
-          <li>
-            <a href="#project">Projects</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
+
+        {/* Menu items */}
+        <ul
+          className={`menuItems ${menuopen ? "menuopen" : ""}`}
+          onClick={() => setMenuopen(false)} // close menu when a link is clicked
+        >
+          <li><a href="#about">About</a></li>
+          <li><a href="#Experience">Experiences</a></li>
+          <li><a href="#project">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
         </ul>
       </div>
     </nav>
