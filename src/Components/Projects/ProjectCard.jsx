980d49ab802fc6893f6ea8a91f8c1ece88823031
rusinduss <rusindunm@gmail.com>
2025-08-15 01:22:14 +0530

    final projects

--- src/Components/Projects/ProjectCard.jsx
+++ src/Components/Projects/ProjectCard.jsx
@@ -3,14 +3,18 @@ import "./ProjectCard.css";
 import CStours from "../../assets/CStours.png";
 import Movieapp from "../../assets/Movieapp.png";
 import CEB from "../../assets/CEB.png";
+import AtDigital from "../../assets/AtDigital.png"
+import FullStack from "../../assets/FullStack.png"
 
 const projectImages = {
   "Movieapp.png": Movieapp,
   "CStours.png": CStours,
-  "CEB.png": CEB
+  "CEB.png": CEB,
+  "AtDigital.png":AtDigital,
+  "FullStack.png":FullStack
 };
 
-export const ProjectCard = ({ project: { title, description, imageSrc, skills, source } }) => {
+export const ProjectCard = ({ project: { title, description, imageSrc, skills, source,demo} }) => {
   return (
     <div className="project-container">
       <img className="project-image" src={projectImages[imageSrc]} alt={title} />
@@ -24,6 +28,7 @@ export const ProjectCard = ({ project: { title, description, imageSrc, skills, s
       <div className="project-links">
         {/* <a className="project-link" href={demo} target="_blank" rel="noopener noreferrer">Demo</a> */}
         <a className="project-link" href={source} target="_blank" rel="noopener noreferrer">Source</a>
+        <a className="project-link" href={demo} target="_blank" rel="noopener noreferrer">Demo</a>
       </div>
     </div>
   );
